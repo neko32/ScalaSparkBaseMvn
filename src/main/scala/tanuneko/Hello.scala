f225f9452ea2429c76bf537cc90ac334ee11389c
neko32 <neko64@outlook.jp>
2016-01-07 23:57:28 -0500

    use local rathar than YARN for now

--- src/main/scala/tanuneko/Hello.scala
+++ src/main/scala/tanuneko/Hello.scala
@@ -1,13 +1,14 @@
 package tanuneko
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by neko32 on 2016/01/07.
   */
 object Hello {
   def main(args:Array[String]): Unit = {
-    def sc = new SparkContext
+    val conf = new SparkConf().setMaster("local").setAppName("MyTest")
+    val sc = new SparkContext(conf)
     val rdd = sc.textFile("/user/neko32/spark/study1/test.txt")
     rdd.flatMap(_.split(","))
     .map((_, 1))
