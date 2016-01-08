383c4a55dc8b081a783e5c78233687b47c4debb9
neko32 <neko64@outlook.jp>
2016-01-07 23:11:23 -0500

    Initial commit

---
+++ src/main/scala/tanuneko/Hello.scala
@@ -0,0 +1,17 @@
+package tanuneko
+
+import org.apache.spark.SparkContext
+
+/**
+  * Created by neko32 on 2016/01/07.
+  */
+object Hello {
+  def main(args:Array[String]): Unit = {
+    def sc = new SparkContext
+    val rdd = sc.textFile("/user/neko32/scala/study1/test.txt")
+    rdd.flatMap(_.split(","))
+    .map((_, 1))
+    .reduceByKey(_ + _)
+    .saveAsTextFile("/user/neko32/scala/study1/result.txt")
+  }
+}
